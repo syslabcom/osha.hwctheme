06c3c85b03bf623ad3a6205cfc36325353be2fe3
Wolfgang Thomas <thomas@syslab.com>
2014-03-27 14:06:19 +0100

    added parameter only_never

--- src/osha/hwctheme/browser/last_login.py
+++ src/osha/hwctheme/browser/last_login.py
@@ -12,6 +12,8 @@ class LastLoginView(BrowserView):
     @@last-login looks for a list of users to include in
     portal_properties.site_properties.last_login_users which can be added
     manually via the ZMI.
+    ?only_never can be specified so that only users who have never logged in
+    are returned
     """
     def __call__(self):
         llt = api.portal.get_tool("loginlockout_tool")
@@ -29,16 +31,24 @@ class LastLoginView(BrowserView):
         most_recent_attempts = {}
         for attempt in successful_attempts:
             username = attempt["username"]
+            if username not in all_users:
+                # we are not interested in this user
+                continue
             date = attempt["attempts"][-1]["last"]
             most_recent_attempts[username] = date
         for user in all_users:
             if user not in most_recent_attempts.keys():
                 most_recent_attempts[user] = "Never"
-        csvfile = StringIO()
-        writer = csv.DictWriter(csvfile, fieldnames=["user", "last_login"])
-        for user in most_recent_attempts:
-            writer.writerow({
-                "user": user,
-                "last_login": most_recent_attempts[user],
-            })
-        return csvfile.getvalue()
+        if "only_never" in self.request.form:
+            return "\n".join([
+                user for user in most_recent_attempts
+                if most_recent_attempts[user] == "Never"])
+        else:
+            csvfile = StringIO()
+            writer = csv.DictWriter(csvfile, fieldnames=["user", "last_login"])
+            for user in most_recent_attempts:
+                writer.writerow({
+                    "user": user,
+                    "last_login": most_recent_attempts[user],
+                })
+            return csvfile.getvalue()
