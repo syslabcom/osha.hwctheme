bd9a22c10156e76ffed679d6fac7cadbe3f09894
Alexander Pilz <pilz@syslab.com>
2014-03-27 12:31:13 +0100

    prepare deployment

---
+++ src/osha/hwctheme/theme/HWManageStress_files/package.sh
@@ -0,0 +1,2 @@
+# install via npm install uglify-js -g
+node_modules/.bin/uglifyjs jquery.js jquery.cookieBar.js bootstrap-transition.js bootstrap-alert.js bootstrap-modal.js bootstrap-dropdown.js bootstrap-scrollspy.js bootstrap-tab.js bootstrap-tooltip.js bootstrap-popover.js bootstrap-button.js bootstrap-collapse.js bootstrap-carousel.js bootstrap-typeahead.js -o hwdeploy.js -c
